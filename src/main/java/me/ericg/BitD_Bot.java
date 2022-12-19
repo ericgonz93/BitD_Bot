06da36a14ca15fbb0cceec9bfca9eb8c64afbc3a
ericgonz93 <ericgonz93@users.noreply.github.com>
2022-12-19 02:00:13 -0600

    Fixing to ignore token class

--- src/main/java/me/ericg/BitD_Bot.java
+++ src/main/java/me/ericg/BitD_Bot.java
@@ -13,15 +13,16 @@ import javax.security.auth.login.LoginException;
 public class BitD_Bot {
     /*private Dotenv config;
 
+
     public BitD_Bot(Dotenv config) {
         this.config = config;
     }*/
 
     public static void main(String[] args) throws LoginException, InterruptedException {
         //config= Dotenv.configure().load();
-        key api =  new key();
+        //key api =  new key();
         //Created a key object in another class to house bot token just to make this work. Will change later.
-        JDA bot = JDABuilder.createDefault(api.getToken())
+        JDA bot = JDABuilder.createDefault("Token")
                 .enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_MEMBERS)
                 .setActivity(Activity.watching("you roll dice"))
                 .addEventListeners(new SpoggListener())
--- src/main/java/me/ericg/key.java
+++
@@ -1,9 +0,0 @@
-package me.ericg;
-
-public class key {
-    String token = "OTU1OTkwNzYyNzc0OTk0OTQ0.Yjptxw.dHujhvu3rJtu-2JEAXKHMbMRBRI";
-
-    String getToken(){
-        return this.token;
-    }
-}
